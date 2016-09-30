19d151afa0e8e06bc1e850de83b35ffd2a8c57d0
Muhammad Hewedy <MuhammadHewedy@users.noreply.github.com>
2016-09-30 20:21:59 +0300

    Rename data.sql to data-h2.sql

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-
--- password Aa123456
-insert into user (username, password, enabled, role, created_by, created_date, modified_by, modified_date)
-	values ('admin', '$2a$10$fmB9/MThUJIjBmXd4cjWc.7le.mZNdnYSeiZp9btAhF3HLl4vPC56', 1, 'ROLE_ADMIN', -1, CURRENT_TIMESTAMP(), -1, CURRENT_TIMESTAMP());
