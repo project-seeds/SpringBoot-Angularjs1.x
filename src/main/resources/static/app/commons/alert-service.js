55c6416c222451f3fe9a7ed1b89bfa6e8af0058e
Muhammad Hewedy <MuhammadHewedy@users.noreply.github.com>
2016-09-30 20:30:38 +0300

    autoscroll

--- src/main/resources/static/app/commons/alert-service.js
+++ src/main/resources/static/app/commons/alert-service.js
@@ -3,29 +3,44 @@
 
 angular.module('myApp')
 
-.factory('AlertService', ['$rootScope', function($rootScope) {
+.factory('AlertService', ['$rootScope', '$location', '$anchorScroll', function($rootScope, $location, $anchorScroll) {
     return {
-        success: function(msg, link) {
+        success: function(msg, link, timeMillis) {
             var alert = {
-                message: msg,
+                message: msg ? msg : 'success',
                 type: 'success',
-                link: link
+                link: link,
+                id: new Date().getTime()
             }
 
-            $rootScope.alerts = [];
-            $rootScope.alerts.push(alert);
+            doAlert($rootScope, $location, $anchorScroll, alert, timeMillis, 1000)
         },
-        error: function(msg, link) {
+        error: function(msg, link, timeMillis) {
             var alert = {
-                message: msg,
+                message: msg ? msg : 'fail',
                 type: 'danger',
-                link: link
+                link: link,
+                id: new Date().getTime()
             }
-            $rootScope.alerts = [];
-            $rootScope.alerts.push(alert);
-        },
-        reset: function() {
-            $rootScope.alerts = [];
+            doAlert($rootScope, $location, $anchorScroll, alert, timeMillis, 5000)
         }
     }
 }]);
+
+function doAlert($rootScope, $location, $anchorScroll, alert, timeMillis, defaultTimeMillis) {
+    var alertId = 'alert.' + alert.id;
+
+    $rootScope.alerts = [];
+    $rootScope.alerts.push(alert);
+    autoHide(alertId, (timeMillis ? timeMillis : defaultTimeMillis))
+
+    $location.hash(alertId);
+    $anchorScroll();
+}
+
+function autoHide(alertId, timeMillis) {
+    setTimeout(function() {
+        console.log($('#alert'));
+        $('#alert').fadeOut('slow')
+    }, timeMillis);
+}
